5c5770bbfe154846c6e2ac01e68ae3b5c23f5846
OmarGR203 <oguzma20@gmial.com>
2023-10-06 08:35:05 -0600

    Actulización 061023

--- src/Hilos/Proceso1.java
+++ src/Hilos/Proceso1.java
@@ -6,22 +6,24 @@ import java.util.Scanner;
 public class Proceso1 extends Thread{
     @Override
     public void run(){
-        Scanner obj=new Scanner(System.in);
-        System.out.println("ACCESO AL SISTEMA");
-        Scanner datos = new Scanner(System.in);
-        int intento, contrasena=2060, acceso = 0, eleccion, opcion;
+       // Scanner obj=new Scanner(System.in);
+        JOptionPane.showMessageDialog(null, "ACCESO AL SISTEMA");
+       // Scanner datos = new Scanner(System.in);
+      //  int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu edad"));
+        int  contrasena=2060, acceso = 0, eleccion, opcion;
         do {
-            System.out.println("INGRESA PIN DE SEGURIDAD");
-            intento = datos.nextInt();
+           int intento = Integer.parseInt(JOptionPane.showInputDialog(null, "INGRESA EL PIN DE SEGURIDAD"));
+
             if (intento == contrasena) {
-                System.out.println("NIP CORRECTO");
-                System.out.println(" BIENVENIDO ");
+                JOptionPane.showMessageDialog(null, "NIP CORRECTO");
+                JOptionPane.showMessageDialog(null, "BIENVENIDO");
             } else {
-                System.out.println("Contraseña incorrecta");
+                JOptionPane.showMessageDialog(null, "Contraseña incorrecta");
                 acceso = acceso + 1;
                 if (acceso == 2) {
-                    System.out.println("¡Acceso restringido!");
-                    System.out.println("Supero el limite de intentos");
+                    JOptionPane.showMessageDialog(null, "Accesso restringido");
+                    JOptionPane.showMessageDialog(null, "Supero el limite de intentos");
+
                 }
             }
         }while (acceso<3);
