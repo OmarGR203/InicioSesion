6f799fb37f346dcfdce08a5469f74c8253208b12
OmarGR203 <oguzma20@gmial.com>
2023-10-07 11:04:29 -0600

    Actulización 071023

--- src/Hilos/ClasePrincipal.java
+++ src/Hilos/ClasePrincipal.java
@@ -1,20 +1,22 @@
 package Hilos;
 
 import javax.swing.*;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
 
 public class ClasePrincipal{
 
     public static void main (String[] args) {
-        Proceso1 hilo1 = new Proceso1();
-//        Thread hilo2 = new Thread((Runnable) new VentanaCronometro());
-    //   VentanaCronometro miVentana = new VentanaCronometro();
-      // miVentana.setVisible(true);
-
-
-      //  VentanaCronometro hilo2 = new VentanaCronometro();
-       hilo1.start();
+       //  Proceso1 hilo1 = new Proceso1();
+       Proceso2 hilo2 = new Proceso2();
         //hilo2.start();
+//        Thread hilo2 = new Thread((Runnable) new VentanaCronometro());
+     VentanaCronometro miVentana = new VentanaCronometro();
+      miVentana.setVisible(true);
 
+        //  VentanaCronometro hilo2 = new VentanaCronometro();
+        //  hilo1.start();
+        hilo2.start();
 
     }
 
